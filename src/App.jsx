7fbfb0f6358cebb6287d32fc42d138bb5113ca07
has673 <hassansajjas>
2024-07-26 22:51:07 +0500

    color

--- src/App.jsx
+++ src/App.jsx
@@ -59,14 +59,14 @@ function App() {
           
           <div className='row' style={{ position: 'absolute', top: `132vh`}}>
             <h2>Be a Man of the Future.</h2>
-            <p style={{ maxWidth: '400px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>
+            <p style={{ maxWidth: '400px', color:"whitesmoke" }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>
             <button>Read more</button>
           </div>
 
           <div className='row' style={{ position: 'absolute', top: `230vh`}}>
             <div className='col' style={{ position: 'absolute', right: `40px`, width: "540px"}}>
               <h2 style={{ maxWidth: "440px" }}>Tech-Savvy Side</h2>
-              <p style={{ maxWidth: '440px' }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>                
+              <p style={{ maxWidth: '440px' ,  color:"whitesmoke" }}>Featuring a sleek, metallic design inspired by advanced technology, this aftershave bottle is as stylish as it is functional. But it's not just a pretty face - inside, you'll find a nourishing and protective aftershave formula that will leave your skin feeling refreshed and hydrated.</p>                
               <button>Read more</button>
             </div>
           </div>
