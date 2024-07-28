2e56acb0e18dc814650c1abe423dff1c2b4ad3ae
has673 <hassansajjas>
2024-07-28 19:11:09 +0500

    shape

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React , {useEffect} from 'react'
-import { ScrollControls, Scroll, Environment,Sparkles,Backdrop,Float, Ring} from '@react-three/drei'
+import { ScrollControls, Scroll, Environment,Sky,Backdrop,Float, Ring,Text3D, Sphere} from '@react-three/drei'
 
 import baffle from 'baffle'
 import { Space } from './components/Space';
+
 
 function App() {
 
   useEffect(()=> {
     const target = baffle('.title')
     target.set({
-      characters: '░P░h░a░n░t░o░m░',
+      characters: '░P░h░a|n&t^o!m}',
       speed: 100
     })
     target.start()
@@ -18,17 +19,22 @@ function App() {
 
   return (
     <>
-      <color attach="background" args={['#333333']} />
+      <color attach="background" args={['#333']} />
       <ambientLight intensity={0.2} />
       <spotLight position={[0, 25, 0]} angle={1.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
       <Environment
-        preset='warehouse'
+        preset='dawn'
+        background
+        
       />
+     
 
       <ScrollControls pages={6} damping={0.1}>
 
         <Space scale={0.8}/>
-        <Sparkles size={5} count={300} color={"#fff"} scale={[10,10,10] }></Sparkles>
+        <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25}  />
+
+       
         <Backdrop
           receiveShadow
           floor={20.5} // Stretches the floor segment, 0.25 by default
@@ -45,9 +51,10 @@ function App() {
           floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
           floatingRange={[1, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
         >
-        <Ring scale={3.5} position-z={-2.5} position-y={-1} args={[0, 0.95, 60]} receiveShadow>
-          <meshStandardMaterial color="#2a3a3f" />
-        </Ring>
+      
+        <Ring scale={3.5} position-z={-2.5} position-y={-1} args={[1.1, 1.95, 40]} receiveShadow>
+          <meshStandardMaterial color="#333" />
+     </Ring>
       
         </Float>              
 
@@ -55,6 +62,7 @@ function App() {
                 
         </Scroll>
         <Scroll html style={{width: '100%'}}>
+
           <h1 className='title' style={{ color: '#cdcbca',position: 'absolute', top: `65vh`,left: '50%', fontSize: '13em', transform: `translate(-50%,-50%)` }}>PHANTOM</h1>
           
           <div className='row' style={{ position: 'absolute', top: `132vh`}}>
